baacc981f6b75e9cdd76a32ff9d6366e478edc41
kirmiziisik <usak.sezer@gmail.com>
2022-11-27 18:00:53 +0300

    about page updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,9 +4,19 @@ const About = () => {
   return (
     <section className="about-section">
       <h1 className="about-title">About Us</h1>
-      <p className="about">
+      <h2 className="about">
         Recipes you want to make & Cooking advice that works...
-      </p>
+      </h2>
+      <div className="container">
+        <img src="./about.avif" alt="" />
+        <p className="about-para">
+          With a crisp and easily navigable site and backing of skilled and
+          experienced culinary masters, this platform furnishes its visitors
+          with thousands of recipes and meal preparation videos. It also
+          features comprehensive how-to-do guides on absolutely everything
+          food-related topics.
+        </p>
+      </div>
     </section>
   );
 };
